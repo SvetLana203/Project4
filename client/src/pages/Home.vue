976b4df59403214adff6dd4adf47dc4077729fa9
SvetLana203 <shirapovauu@gmail.com>
2021-10-18 10:13:15 -0500

    home

--- client/src/pages/Home.vue
+++ client/src/pages/Home.vue
@@ -3,6 +3,7 @@
 <v-card class="text-center">
   <h2>ZeroWaste</h2>
 </v-card>
+<div class="font-weight-black">Give your stuff the second life. Sell or donate it. Do not waste it </div>
 </v-app>
 </template>
 <script>
